27839ce7c05a94af8dc40e5371e794feb7a167d4
Paul Chana <pchana11@hotmail.com>
2020-08-17 10:04:34 -0500

    redid Code quiz Homework

---
+++ index.js
@@ -0,0 +1,13 @@
+var timer = 
+var question = 
+var score = 
+var submitButton = 
+var eraseButton = 
+var startButton = 
+var resetButton = 
+
+
+// somehow im supposed to use Javascript and type some stuff out that will get this game working.
+// its been some time but I still dont have any understanding of this. There are supposed to be Questions which i think i did right.
+// there ais supposed to be a some timer that is counting down in some corner 
+// there shoud be some styling and things to make the page look good.
---
+++ questions.js
@@ -0,0 +1,40 @@
+var questions = [
+    {
+      title: "What board game do you like the most?:",
+      choices: ["Monopoly", "Sorry", "Trouble", "Snakes and Ladders"],
+      answer: "Monopoly"
+    },
+    {
+      title: "What's something that you recently learned that everyone else already knew?:",
+      choices: ["HTML", "CSS", "Javascript", "None of the Above"],
+      answer: "None of the Above"
+    },
+    {
+      title: "An adventure doing one of the following sounds best?:",
+      choices: [
+        "Skydiving",
+        "Scuba Diving",
+        "Safari",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      title:
+        "What is the best movie ever?:",
+      choices: ["Jurassic Park", "The Fifth Element", "Big", "Patch Adams"],
+      answer: "The Fifth Element"
+    },
+    {
+      title:
+        "What's the funniest TV series you have seen?:",
+      choices: ["Friends", "HIMYM", "Everybody Loves Raymond", "The Big Bang Theory"],
+      answer: "Trick Question, Answer: That 70's Show"
+    },
+    {
+        title:
+          "What's the best way to travel?:",
+        choices: ["Car", "Train", "Plane", "Ship"],
+        answer: "Train"
+    }
+  ];
